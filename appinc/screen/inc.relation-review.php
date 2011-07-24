333e7cea610edb439e6952e190245e026032ab0a
Pirhoo <pierre@owni.fr>
2011-07-24 15:34:09 +0200

    Align submit buttons

--- appinc/screen/inc.relation-review.php
+++ appinc/screen/inc.relation-review.php
@@ -75,7 +75,7 @@
     $s->assign("countRelation", $managers["relation"]->getRelationCount());
     $s->assign("countRelationReview", $managers["relation_trust_level"]->getRelationTrustLevelCount());
     $s->assign("countUser", $managers["user"]->getUserCount());
-
+    
     // every errors to a JSON
     $s->assign("err_json", json_encode($err));
     $s->display('index.tpl');
